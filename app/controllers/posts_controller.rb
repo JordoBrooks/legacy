317531f186dc27882d9654f4d5840a0a7c4047d9
Jordan Brooks <jordbrooks14@gmail.com>
2017-08-28 15:44:04 -0700

    Uploaded images + captions saved to db, and saved images displayed on index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all
 	end
 	
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(params_for_post)
+
+		redirect_to posts_path
+	end
+
+	private
+
+	def params_for_post
+		params.require(:post).permit(:image, :caption)
+	end
+
 end
