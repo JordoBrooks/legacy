4273cc52d61a573c82d6acb52b949480564b3322
Jordan Brooks <jordbrooks14@gmail.com>
2017-11-02 00:28:51 -0700

    Make post upload/edit pages nicer to look at. Add javascript to handle image upload preview.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+  def display_form_image_or_placeholder(post)
+    if post.image.exists?
+      image_tag(post.image.url(:medium), id: 'image-preview', class: 'img-responsive ')
+    else
+      image_tag('placeholder-post-img.jpg', id: 'image-preview', class: 'img-responsive ')
+    end
+  end
+
 end
