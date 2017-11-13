64992e244c135005b58d43f6ff416d988ec9765a
Jordan Brooks <jordbrooks14@gmail.com>
2017-11-13 02:01:10 -0800

    Add pagination to user/show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @posts = @user.posts
+    @posts = @user.posts.order('created_at DESC').page params[:page]
   end
 
   def destroy
