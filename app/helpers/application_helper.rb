3e4a816e043885d8b914c57fda2aaf32cc8d3c87
Jordan Brooks <jordbrooks14@gmail.com>
2017-11-20 01:46:49 -0800

    Fix styling issues.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,18 +9,33 @@ module ApplicationHelper
   end
 
   def profile_pic_select(user, context)
-    if context == 'edit'
+    if context == 'profile'
       if user.image.exists?
         return image_tag user.image.url(:large),
                          id: 'image-preview',
                          class: 'img-responsive img-circle profile-image'
       end
+
+      if current_user?(user)
+        image_tag 'default-user-image-edit.jpg', id: 'image-preview',
+                  class: 'img-responsive img-circle profile-image'
+      else
+        image_tag 'default-user-image.jpg', id: 'image-preview',
+                  class: 'img-responsive img-circle profile-image'
+      end
+
+    elsif context == 'edit'
+      if user.image.exists?
+        return image_tag user.image.url(:large),
+                         id: 'image-preview',
+                         class: 'img-responsive img-circle edit-profile-image'
+      end
       image_tag 'default-user-image-edit.jpg', id: 'image-preview',
-                class: 'img-responsive img-circle profile-image'
+                class: 'img-responsive img-circle edit-profile-image'
+
     elsif context == 'post'
       if user.image.exists?
         return image_tag user.image.url(:thumb),
-                         id: 'image-preview',
                          class: 'img-responsive img-circle post-top-img'
       end
       image_tag 'default-user-image-post.jpg',class: 'img-responsive img-circle post-top-img'
